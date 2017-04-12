ef70a2d988f1629682b63663df002f1617dc7d87
jiwon <jiwonh@gmail.com>
2017-04-11 18:45:04 -0700

    Basic server setup

---
+++ server/config/mongoose.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp').then(() => {
+  console.log('Connected to MongoDB Server.');
+},
+err => {
+  console.log('Mongodb connection error: ', err);
+});
+
+module.exports = {mongoose};
---
+++ server/server.js
@@ -0,0 +1,28 @@
+var {mongoose} = require('./config/mongoose');
+var {User} = require('./models/User');
+var {Todo} = require('./models/Todo');
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var port = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (err) => {
+    res.status(400).send(err);
+  });
+});
+
+app.listen(port, () => {
+    console.log('Server is running on port ' + port);
+});
