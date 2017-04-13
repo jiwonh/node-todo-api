340160f0741d0b085d9c02329c206d356900516f
jiwon <jiwonh@gmail.com>
2017-04-12 18:15:50 -0700

    Add tests for GET /todos

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -2,10 +2,10 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp').then(() => {
-  console.log('Connected to MongoDB Server.');
+  //console.log('Connected to MongoDB Server.');
 },
 err => {
-  console.log('Mongodb connection error: ', err);
+  //console.log('Mongodb connection error: ', err);
 });
 
 module.exports = {mongoose};
